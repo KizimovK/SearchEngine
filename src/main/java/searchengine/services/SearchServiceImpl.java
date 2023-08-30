a9264b6415d21e70cff8cba0d9627ceda6e1b164
konstantin_kizimov <kizim_net@mail.ru>
2023-08-30 18:13:06 +0400

    Sorting pages by relevance

--- src/main/java/searchengine/services/SearchServiceImpl.java
+++ src/main/java/searchengine/services/SearchServiceImpl.java
@@ -1,6 +1,7 @@
 package searchengine.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.AssertionFailure;
 import org.springframework.stereotype.Service;
 import searchengine.companets.ExtractLemma;
 import searchengine.dto.searchs.SearchDto;
@@ -14,6 +15,8 @@ import searchengine.repository.LemmaRepository;
 import searchengine.repository.PageRepository;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -44,16 +47,27 @@ public class SearchServiceImpl implements SearchService {
 
     private List<SearchDto> getResultSearchFromOneSite(List<String> lemmasQueryList, String site) {
         SiteDto siteDto = siteService.findSite(site);
-        Map<PageEntity, Float> pageRelevanceMap = new HashMap<>();
+        Map<PageEntity, Float> pageRelevanceAbsMap = new HashMap<>();
         List<LemmaEntity> lemmasList = getLemmasFromSite(lemmasQueryList, siteDto);
         List<PageEntity> pagesList = pageRepository.findPagesByLemmas(lemmasList);
         for (PageEntity pageEntity : pagesList){
             List<IndexEntity> indexEntityList = indexRepository.findIndexByPageAndLemmasList(pageEntity,lemmasList);
             if (indexEntityList!=null){
-                indexEntityList.forEach(indexEntity -> pageRelevanceMap.merge(pageEntity,
+                indexEntityList.forEach(indexEntity -> pageRelevanceAbsMap.merge(pageEntity,
                         indexEntity.getRank(), (k, v) -> v + indexEntity.getRank()));
             }
         }
+        float maxRelevancePage = pageRelevanceAbsMap.values().stream().max(Float::compareTo).get();
+        pageRelevanceAbsMap.keySet().forEach(pageEntity ->
+                pageRelevanceAbsMap.compute(pageEntity,(v,k)->k/maxRelevancePage));
+        Map<PageEntity, Float> pageRelevanceMap = pageRelevanceAbsMap.entrySet().stream().
+                sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).
+                collect(Collectors.toMap(
+                        Map.Entry::getKey,Map.Entry::getValue,
+                        (aFloat, aFloat2) -> aFloat, LinkedHashMap::new));
+
+
+
         log.info("End search!");
         return null;
     }
